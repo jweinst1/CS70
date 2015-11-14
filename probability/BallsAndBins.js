6945d5f2f806da9da177d95726ad6decc7128256
Joshua Weinstein <jweinst1@berkeley.edu>
2015-11-14 00:59:39 -0800

    more prob functions

--- probability/BallsAndBins.js
+++ probability/BallsAndBins.js
@@ -42,21 +42,59 @@ function indist_to_bin (balls, bins) {
     while (machine.hasnext()) bin[rand_interval(0, bin.length-1)].push(machine.next());
     return bin;
 }
-
+//returns a randomly selected key from an object
+function rand_obj_key(obj) {
+    var result = Object.keys(obj);
+    return result[rand_interval(0, result.length-1)];
+}
 /* sets up a process similar to above, but with a distinct set of bins as an object. */
 function to_dist_bin (balls, bins) {
     var bin = new distbins(bins);
     var machine = new balliter(balls);
-    while (machine.hasnext()) bin[Object.keys(bin)[rand_interval(0, bin.length-1)]].push(machine.next());
+    while (machine.hasnext()) {
+        var select = rand_obj_key(bin);
+        bin[select].push(machine.next());
+    }
     return bin;
 }
+/*console.log(to_dist_bin(6, 6));
+ { '0': [ '*', '*' ],
+ '1': [],
+ '2': [ '*' ],
+ '3': [ '*', '*' ],
+ '4': [ '*' ],
+ '5': [] }
+ */
 
+//Object that generates a coin toss everytime the flip method is called.
+var cointosser = function() {
+    this.coins = ["H", "T"];
+    this.flip = function() {
+        return this.coins[rand_interval(0, 2)];
+    };
+    this.trial_flips = function(amount) {
+        var results = "";
+        for(i=0;i<amount;i++) results += this.flip();
+        return results;
+    };
+};
+/*
+var c = new cointosser();
+console.log(c.trial_flips(10));
+ TTHHHHHTTT
+*/
 
-
-
-
-
-
+//runs a series of coin toss trials, of length n, and returns the resutls as a object that counts each event.
+function coin_toss_run(trials, n) {
+    var tosser = new cointosser();
+    var results = {};
+    for(i=0;i<trials;i++) {
+        var toss = tosser.trial_flips(n);
+        if(toss in results) results[toss] += 1;
+        else results[toss] = 1;
+    }
+    return results;
+}
 
 
 //rand number functions------>>>>
@@ -71,3 +109,4 @@ function rand_interval(x, y) {
         return Math.floor((Math.random() * y) + x);
     }
 }
+// ------->>>>end
---
+++ probability/cond_probability.js
@@ -0,0 +1,49 @@
+
+//counts the occurences of an element in an array
+function count_elem(lst, elem) {
+    var count = 0;
+    for(var num in lst) if(lst[num] === elem) count++;
+    return count;
+}
+//calculates the prob of picking an element from an array
+function picking_prob(lst, elem) {
+    var prob = count_elem(lst, elem)/lst.length;
+    return prob;
+}
+/*console.log(picking_prob([1, 2, 3, 3, 3, 3, 3, 3], 2));
+ 0.125
+*/
+
+//Object that gives the individual probabilities for picking each element in an array
+var probability_set = function(lst) {
+    for(var num in lst) this[lst[num]] = picking_prob(lst, lst[num]);
+};
+/*
+console.log(new probability_set([1, 2, 2, 4, 5]));
+ { '1': 0.2, '2': 0.4, '4': 0.2, '5': 0.2 }
+*/
+
+
+//calculates the conditional probability of picking elem, given first has
+//already been picked.
+function cond_pick_prob(lst, first, elem) {
+    lst.pop(first);
+    var prob = count_elem(lst, elem)/lst.length;
+    return prob;
+}
+
+
+
+//rand number functions------>>>>
+//returns a number from 1 to num randomly.
+function randrange(num) {
+    return Math.floor((Math.random() * num) + 1);
+}
+//returns a number from x to y randomly, throws an error if x is y.
+function rand_interval(x, y) {
+    if(x===y) throw "Value Error";
+    else {
+        return Math.floor((Math.random() * y) + x);
+    }
+}
+// ------->>>>end
